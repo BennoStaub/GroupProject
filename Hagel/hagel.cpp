78899fbfbdb0d1b57b511e6288aaa42e8e61ed06
BennoStaub <staubbe@student.ethz.ch>
2017-09-15 18:01:49 +0200

    hagels actual version

---
+++ Hagel/hagel.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+string question1 = "Hello, whats yo name? ";
+string question2 = "How are ya nigga? ";
+string question3 = "Where ya house livin at? ";
+string question4 = "Whatcha up to? ";
+string question5 = "Getting fucked up tonight? ";
+string question6 = "Waddup dawg? ";
+string answer1 = "Shaqueisha nigga.";
+string answer2 = "Fine af ma dude.";
+string answer3 = "Bronx mofo.";
+string answer4 = "Gonna go party heavyyyy.";
+string answer5 = "Damn sure homie.";
+string answer6 = "Ballin'.";
+
+
+	cout << question1;
+	cout << answer1 << endl;
+	cout << question2;
+	cout << answer2 << endl;
+	cout << question3;
+	cout << answer3 << endl;
+	cout << question4;
+	cout << answer4 << endl;
+	cout << question5;
+	cout << answer5 << endl;
+	cout << question6;
+	cout << answer6 << endl;
+
+string mystr;
+	cout << "What's your name?" << endl;
+	getline (cin, mystr);
+	cout << "Hello " << mystr << ", How old are you?" << endl;
+	getline (cin, mystr);
+	cout << "I'm " << mystr << " years old too!" << endl;
+
+
+
+	cout << "Hellooo bitch ass nigga." << endl;
+		cout << "Wanna learn some shit bitch? Swing first!" << endl;
+		cout << "Type yo translation bullcrap into this motherfucker and press enter too see if youre right ma dude." << endl;
+			string i;
+		cout << "Quality - " ;
+		cin  >>	i;
+	if (i == "Qualität") {
+		cout << "Damn thats right hoe ass nigga." << endl;
+	} else {
+		cout << "Hä ä machsh mo nüm sowas." << endl;
+		cout << "Yo nasty ass shoulda typed 'Qualität'." << endl;
+	}
+return 0;
+}
