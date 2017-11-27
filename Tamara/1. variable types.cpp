df28ec0c61d1665027527e97bb9dde4592bdb30b
tamaraegger <31291736+tamaraegger@users.noreply.github.com>
2017-11-27 21:02:30 +0100

    Aufgabe 1 - "variable types"

---
+++ Tamara/1. variable types.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a=5;
+	int b=3;
+	int result;
+
+	result = a+b;
+	cout << result << endl;
+
+{string mystring;
+mystring = "My name is Tamara";
+cout << mystring << endl;}
+
+{float myfloat;
+ float a=5;
+ float b=7;
+ myfloat = a/b;
+ cout << myfloat << endl;}
+
+{char mychar;
+mychar = '$';
+cout << mychar << endl;}
+
+{bool mybool;
+mybool = true;
+cout << mybool;}
+
+	return 0;
+}
