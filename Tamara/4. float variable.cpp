8a174f424b220f671fd41c601653f85f839b75e2
tamaraegger <31291736+tamaraegger@users.noreply.github.com>
2017-11-27 21:03:14 +0100

    Aufgabe 4 - "float variable"

---
+++ Tamara/4. float variable.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+	{float myfloat;
+	 myfloat = 3.1415926535;
+	 cout << myfloat << endl;}
+
+	{long double mydouble;
+	 mydouble = 3.1415926535;
+	 cout << mydouble << endl;}
+
+
+	return 0;
+}
+
