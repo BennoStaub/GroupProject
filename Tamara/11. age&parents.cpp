23d091b07d81e96b7e6cef3b7a0995a6c94a9a6b
tamaraegger <31291736+tamaraegger@users.noreply.github.com>
2017-11-27 21:04:10 +0100

    Aufgabe 11 - "age&parents"

---
+++ Tamara/11. age&parents.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int age;
+
+cout << "How old are you?" << endl;
+cin >> age;
+
+if (age >= 18){
+cout << "Get in" << endl;
+}else{
+cout << "No, Sorry" << endl;}
+
+	return 0;
+}
+
